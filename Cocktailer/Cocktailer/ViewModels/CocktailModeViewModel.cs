75d7a89b299d18915cced1ca1365c9c0ad7b53e3
Oliver Kolodzik <kolodzik@mail.upb.de>
2020-04-22 16:48:20 +0200

    Uncomment connection method call

--- Cocktailer/Cocktailer/ViewModels/CocktailModeViewModel.cs
+++ Cocktailer/Cocktailer/ViewModels/CocktailModeViewModel.cs
@@ -60,10 +60,10 @@ namespace Cocktailer.ViewModels
             Config = config;
             try
             {
-                //if (!await btService.Init())
-                //{
-                //    throw new Exception();
-                //}
+                if (!await btService.Init())
+                {
+                    throw new Exception();
+                }
             }
             catch (Exception)
             {
